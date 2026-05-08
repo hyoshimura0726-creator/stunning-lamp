0249a7508fcfcde0c0d5ad95b0e7f14a4555890c
yoshimura <h.yoshimura0726@gmail.com>
2026-05-08 22:00:53 +0900

    Fix Supabase anon key

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
 
 const supabaseUrl = 'https://zxkcyptcqbyxutbudeum.supabase.co';
-const supabaseAnonKey = 'sb_publishable_6c1eOKbxyHUT0CRTSm0Rfw_-_lCwViM';
+const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inp4a2N5cHRjcWJ5eHV0YnVkZXVtIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzc4MDkwMjEsImV4cCI6MjA5MzM4NTAyMX0.xPeQl848ywcBF8QszRxjCTHFw-oxr1pHd8ZlUk7QTHI';
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
